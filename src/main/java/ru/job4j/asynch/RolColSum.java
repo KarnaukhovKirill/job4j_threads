2965f3a9f08f4038879e78490cf583505861597b
KarnaukhovKirill <kirya2361@gmail.com>
2021-12-13 10:34:22 +0300

    CompletableFuture. Исправление

--- src/main/java/ru/job4j/asynch/RolColSum.java
+++ src/main/java/ru/job4j/asynch/RolColSum.java
@@ -2,6 +2,9 @@ package ru.job4j.asynch;
 
 import java.util.concurrent.CompletableFuture;
 
+/**
+ * Класс содержит две реализации для подсчёта суммы числе в ряду и вертикали.
+ */
 public class RolColSum {
     public static class Sums {
         private int rowSum;
@@ -29,23 +32,25 @@ public class RolColSum {
         }
     }
 
+    /**
+     * Метод подсчитывает сумму чисел в ряду и вертикали последовательно
+     * @param matrix матрица
+     * @return массив, содержащий сумму числе в i ряду и вертикали
+     */
     public static Sums[] sum(int[][] matrix) {
         Sums[] sums = new Sums[matrix.length];
-        int rowSum = 0;
-        int colSum = 0;
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
-                rowSum += matrix[i][j];
-                colSum += matrix[j][i];
-
-            }
-            sums[i] = new Sums(rowSum, colSum);
-            rowSum = 0;
-            colSum = 0;
+            sums[i] = count(matrix, i);
         }
         return sums;
     }
 
+    /**
+     * Метод подсчитывает сумму чисел в ряду и вертикали с использованием асинхронного подсчёта
+     * @param matrix матрица
+     * @return массив, содержащий сумму числе в i ряду и вертикали
+     * @throws Exception exception
+     */
     public static Sums[] asyncSum(int[][] matrix) throws Exception {
         Sums[] sums = new Sums[matrix.length];
         for (int i = 0; i < matrix.length; i++) {
@@ -54,15 +59,29 @@ public class RolColSum {
         return sums;
     }
 
+    /**
+     * Метод запускает асинхронную задачу
+     * @param matrix матрица
+     * @param start с какого ряда и вертикали начать подсчёт суммы
+     * @return объект CompletableFuture
+     */
     public static CompletableFuture<Sums> getTask(int[][] matrix, int start) {
-        return CompletableFuture.supplyAsync(() -> {
-            int rowSum = 0;
-            int colSum = 0;
-            for (int i = 0; i < matrix.length; i++) {
-                rowSum += matrix[start][i];
-                colSum += matrix[i][start];
-            }
-            return new Sums(rowSum, colSum);
-        });
+        return CompletableFuture.supplyAsync(() -> count(matrix, start));
+    }
+
+    /**
+     * Алгоритм подсчёта суммы в матрице
+     * @param matrix матрица
+     * @param start с какого ряда и вертикали начать подсчёт суммы
+     * @return модель данных Sums, в котором храниться сумма числе в ряду и вертикали
+     */
+    private static Sums count(int[][] matrix, int start) {
+        int rowSum = 0;
+        int colSum = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            rowSum += matrix[start][i];
+            colSum += matrix[i][start];
+        }
+        return new Sums(rowSum, colSum);
     }
 }
